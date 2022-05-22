03c41b1ad874c9f29840341d07f6db40cf4dcc87
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-22 16:03:51 -0300

    add buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+ <v-container>
+    <v-layout row wrap>
+        <v-flex xs12 sm6 class="text-center text-sm-right">
+        <v-btn large router style="margin: 10px;" to="/meetup">Explore Meetups</v-btn>
+        </v-flex>
+        <v-flex xs12 sm6 class="text-center text-sm-left">
+        <v-btn large router style="margin: 10px;" to="/createmeetup">Organize Meetups</v-btn>
+        </v-flex>
+    </v-layout>
+ </v-container>
 </template>
